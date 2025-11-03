811555c5f305f732b229b076ed339d9881b65ae9
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-11-03 07:26:55 -0300

    Creacion de las funciones fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,25 @@
+function fibs(num) {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    if (i < 2) {
+      arr.push(i);
+    } else {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+  }
+
+  return arr;
+}
+
+function fibsRec(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+  const arr = fibsRec(num - 1);
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return arr;
+}
+
+console.log(fibs(8));
+console.log(fibsRec(8));
